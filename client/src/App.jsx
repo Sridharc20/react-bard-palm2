eb2abf5421b5ad893c257dcde636558b012e0f80
sridharc20 <sridharc20@gmail.com>
2023-12-03 19:07:08 +0530

    Added server api call

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,20 +1,37 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [serverData, setServerData] = useState('')
 
+
+  useEffect(() => {
+    fetch("/api")
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data)
+        setServerData(data)
+      }
+      )
+  }, [])
+
+  function submitPrompt() {
+    debugger
+    alert('submitted')
+  }
 
   return (
     <main style={{ height: '100vh', display: 'flex', flexDirection: 'column' }}>
-      <h1 style={{ padding: '10px', marginBottom: '0' }}>MyPrompter</h1>
-      <div style={{margin: '0', flexGrow: '1'}}>
-        <div style={{width: '100%', height: '100%'}}>
-          <article  style={{margin: '0'}} />
+      <h1 style={{ padding: '10px', marginBottom: '0' }}>My Prompter App</h1>
+      <div style={{ margin: '0', flexGrow: '1' }}>
+        <div style={{ width: '100%', height: '100%' }}>
+          <article style={{ margin: '0' }} >
+            {serverData}
+          </article>
         </div>
       </div>
-      <div style={{display: 'flex', alignItems:'end', backgroundColor: '#222', padding: '10px'}}>
-        <textarea style={{margin: '0', flexGrow: '1', overflow: 'hidden'}} placeholder='Type in Prompt' />
-        <button style={{margin: '0', flex: '1', paddingLeft: '10px'}}>Go</button>
+      <div style={{ display: 'flex', alignItems: 'end', backgroundColor: '#222', padding: '10px' }}>
+        <textarea style={{ margin: '0', flexGrow: '1', overflow: 'hidden' }} placeholder='Type in Prompt' />
+        <button style={{ margin: '0', flex: '1', paddingLeft: '10px' }} onSubmit={submitPrompt}>Go</button>
       </div>
     </main>
   )
